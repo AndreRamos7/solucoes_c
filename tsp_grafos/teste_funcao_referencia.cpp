89e6a15cd947b11656ec3219bac7d9fb47f8cd1a
André Ramos <andre-rammos@hotmail.com>
2023-06-05 02:31:23 -0300

    modificações em teste_funcao_referencia.cpp

--- tsp_grafos/teste_funcao_referencia.cpp
+++ tsp_grafos/teste_funcao_referencia.cpp
@@ -3,10 +3,21 @@
 using namespace std;
 
 int main(){
-    int n = 10;
-    vector<bool> verify(n, false);
+    int n = 100;
+    vector<int> verify;
 
-    for(int i = 0; i < verify.size(); i++){
+    cout << "Antes de preencher" << endl;
+    cout << "Verify size() = " << verify.size() << endl;
+    for(int i = 0; i < verify.size(); ++i){
+        cout << verify.at(i) << endl;
+    }
+
+    cout << "preenchendo" << endl;
+    for(int i = 0; i < n; i++){
+        verify.push_back(i*2);
+    }
+    cout << "Verify size() = " << verify.size() << endl;
+    for(int i = 0; i < verify.size(); ++i){
         cout << verify.at(i) << endl;
     }
     return 0;
